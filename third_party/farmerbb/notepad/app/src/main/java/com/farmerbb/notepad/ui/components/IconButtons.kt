3bc7fb5f76cafb754fbe809e3c9e43afb1b15dcf
Tec-kio GAM Cell <gam-cell@tec-kio.local>
2026-06-09 22:56:41 -0600

    vendor: import farmerbb Notepad for analysis

---
+++ third_party/farmerbb/notepad/app/src/main/java/com/farmerbb/notepad/ui/components/IconButtons.kt
@@ -0,0 +1,186 @@
+/* Copyright 2021 Braden Farmer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.farmerbb.notepad.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.PlaylistAddCheck
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material.icons.filled.SdCard
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.SelectAll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.LayoutDirection
+import com.farmerbb.notepad.R
+
+@Composable
+fun BackButton(onClick: () -> Unit = {}) {
+    val imageVector = when(LocalLayoutDirection.current) {
+        LayoutDirection.Rtl -> Icons.AutoMirrored.Filled.ArrowForward
+        else -> Icons.AutoMirrored.Filled.ArrowBack
+    }
+
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null,
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun EditButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.Edit,
+            contentDescription = stringResource(R.string.action_edit),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun SaveButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.Save,
+            contentDescription = stringResource(R.string.action_save),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun DeleteButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.Delete,
+            contentDescription = stringResource(R.string.action_delete),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun MoreButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.MoreVert,
+            contentDescription = null,
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun MultiSelectButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.PlaylistAddCheck,
+            contentDescription = stringResource(R.string.action_start_selection),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun SelectAllButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.SelectAll,
+            contentDescription = stringResource(R.string.action_select_all),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun ExportButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.SdCard,
+            contentDescription = stringResource(R.string.action_export),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun SearchTextField(
+    searchTerm: String,
+    onSearchTermChanged: (String) -> Unit,
+) {
+    val focusRequester = remember { FocusRequester() }
+
+    TextField(
+        value = searchTerm,
+        onValueChange = { newSearchTerm -> onSearchTermChanged(newSearchTerm) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = stringResource(R.string.action_search_notes),
+                tint = Color.White
+            )
+        },
+        label = { Text(stringResource(R.string.action_search_notes)) },
+        singleLine = true,
+        maxLines = 1,
+        modifier = Modifier.fillMaxWidth().focusRequester(focusRequester),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.White,
+            unfocusedBorderColor = Color.White,
+            textColor = Color.White,
+            placeholderColor = Color.White,
+            cursorColor = Color.White,
+            focusedLabelColor = Color.White,
+            unfocusedLabelColor = Color.White
+        )
+    )
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+}
+
+@Composable
+fun SearchNotesButton(onClick: () -> Unit = {}) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(R.string.action_search_notes),
+            tint = Color.White
+        )
+    }
+}
---
+++ third_party/farmerbb/notepad/app/src/main/java/com/farmerbb/notepad/usecase/ArtVandelay.kt
@@ -0,0 +1,237 @@
+/* Copyright 2022 Braden Farmer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.farmerbb.notepad.usecase
+
+import android.net.Uri
+import com.farmerbb.notepad.model.ExportedNotesDirectory
+import com.farmerbb.notepad.model.FilenameFormat
+import com.farmerbb.notepad.model.FilenameFormat.TimestampAndTitle
+import com.farmerbb.notepad.model.FilenameFormat.TitleAndTimestamp
+import com.farmerbb.notepad.model.FilenameFormat.TitleOnly
+import com.farmerbb.notepad.model.Note
+import com.farmerbb.notepad.model.NoteMetadata
+import com.github.k1rakishou.fsaf.FileChooser
+import com.github.k1rakishou.fsaf.FileManager
+import com.github.k1rakishou.fsaf.callback.FileChooserCallback
+import com.github.k1rakishou.fsaf.callback.FileCreateCallback
+import com.github.k1rakishou.fsaf.callback.FileMultiSelectChooserCallback
+import com.github.k1rakishou.fsaf.callback.directory.DirectoryChooserCallback
+import com.github.k1rakishou.fsaf.file.FileSegment
+import org.koin.core.module.dsl.new
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.Date
+import org.koin.dsl.module
+
+interface ArtVandelay {
+    fun importNotes(
+        onNotesSelected: (Int) -> Unit,
+        saveImportedNote: (InputStream, String) -> Unit,
+    )
+
+    fun exportNotes(
+        hydratedNotes: List<Note>,
+        filenameFormat: FilenameFormat,
+        onCancel: () -> Unit,
+        saveExportedNote: (OutputStream, String) -> Unit,
+    )
+
+    fun importAllNotes(
+        saveImportedNotes: (InputStream) -> Unit,
+    )
+
+    fun exportAllNotes(
+        saveExportedNotes: (OutputStream) -> Unit,
+    )
+
+    fun exportSingleNote(
+        metadata: NoteMetadata,
+        filenameFormat: FilenameFormat,
+        saveExportedNote: (OutputStream) -> Unit,
+    )
+}
+
+private class ArtVandelayImpl(
+    private val fileChooser: FileChooser,
+    private val fileManager: FileManager
+): ArtVandelay {
+    override fun importNotes(
+        onNotesSelected: (Int) -> Unit,
+        saveImportedNote: (InputStream, String) -> Unit,
+    ) = fileChooser.openChooseMultiSelectFileDialog(
+        importCallback(onNotesSelected, saveImportedNote)
+    )
+
+    override fun importAllNotes(
+        saveImportedNotes: (InputStream) -> Unit,
+    ) = fileChooser.openChooseFileDialog(
+        importAllCallback(saveImportedNotes)
+    )
+
+    override fun exportAllNotes(
+        saveExportedNotes: (OutputStream) -> Unit,
+    ) = fileChooser.openCreateFileDialog(
+        fileName = generateExportFilename(),
+        fileCreateCallback = exportAllCallback(saveExportedNotes)
+    )
+
+    override fun exportNotes(
+        hydratedNotes: List<Note>,
+        filenameFormat: FilenameFormat,
+        onCancel: () -> Unit,
+        saveExportedNote: (OutputStream, String) -> Unit,
+    ) = fileChooser.openChooseDirectoryDialog(
+        directoryChooserCallback = exportFolderCallback(
+            hydratedNotes,
+            filenameFormat,
+            onCancel,
+            saveExportedNote,
+        )
+    )
+
+    override fun exportSingleNote(
+        metadata: NoteMetadata,
+        filenameFormat: FilenameFormat,
+        saveExportedNote: (OutputStream) -> Unit,
+    ) = fileChooser.openCreateFileDialog(
+        fileName = generateFilename(metadata, filenameFormat),
+        fileCreateCallback = exportFileCallback(saveExportedNote)
+    )
+
+    private val registeredBaseDirs = mutableListOf<Uri>()
+
+    private fun importCallback(
+        onNotesSelected: (Int) -> Unit,
+        saveImportedNote: (InputStream, String) -> Unit,
+    ) = object : FileMultiSelectChooserCallback() {
+        override fun onResult(uris: List<Uri>) {
+            onNotesSelected(uris.size)
+            with(fileManager) {
+                for (uri in uris) {
+                    fromUri(uri)?.let { file ->
+                        val filename = file.getFullPath() // Extract filename from the file
+
+                        getInputStream(file)?.let { input ->
+                            saveImportedNote(input, filename)
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun onCancel(reason: String) = Unit // no-op
+    }
+
+    private fun importAllCallback(
+        saveImportedNotes: (InputStream) -> Unit,
+    ) = object : FileChooserCallback() {
+        override fun onResult(uri: Uri) {
+            with(fileManager) {
+                fromUri(uri)?.let(::getInputStream)?.let { input ->
+                    saveImportedNotes(input)
+                }
+            }
+        }
+
+        override fun onCancel(reason: String) = Unit // no-op
+    }
+
+    private fun exportAllCallback(
+        saveExportedNotes: (OutputStream) -> Unit,
+    ) = object : FileCreateCallback() {
+        override fun onResult(uri: Uri) {
+            with(fileManager) {
+                fromUri(uri)?.let(::getOutputStream)?.let { output ->
+                    saveExportedNotes(output)
+                }
+            }
+        }
+
+        override fun onCancel(reason: String) = Unit // no-op
+    }
+
+
+    private fun exportFolderCallback(
+        hydratedNotes: List<Note>,
+        filenameFormat: FilenameFormat,
+        onCancel: () -> Unit,
+        saveExportedNote: (OutputStream, String) -> Unit,
+    ) = object: DirectoryChooserCallback() {
+        override fun onResult(uri: Uri) {
+            with(fileManager) {
+                if (!registeredBaseDirs.contains(uri)) {
+                    registerBaseDir<ExportedNotesDirectory>(ExportedNotesDirectory(uri))
+                    registeredBaseDirs.add(uri)
+                }
+
+                newBaseDirectoryFile<ExportedNotesDirectory>()?.let { baseDir ->
+                    for (note in hydratedNotes) {
+                        val filename = generateFilename(note.metadata, filenameFormat)
+                        create(baseDir, FileSegment(filename))
+                            ?.let(::getOutputStream)
+                            ?.let { output ->
+                                saveExportedNote(output, note.text)
+                            }
+                    }
+                }
+            }
+        }
+
+        override fun onCancel(reason: String) = onCancel()
+    }
+
+    private fun exportFileCallback(
+        saveExportedNote: (OutputStream) -> Unit,
+    ) = object: FileCreateCallback() {
+        override fun onResult(uri: Uri) {
+            with(fileManager) {
+                fromUri(uri)?.let(::getOutputStream)?.let(saveExportedNote)
+            }
+        }
+
+        override fun onCancel(reason: String) = Unit // no-op
+    }
+
+    private fun generateExportFilename(): String {
+        val title = "notepad_backup"
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd-HH-mm", Locale.getDefault())
+        val timestamp = dateFormat.format(Date())
+        val filename = "${title}_$timestamp"
+
+        return "$filename.json"
+    }
+
+    private fun generateFilename(
+        metadata: NoteMetadata,
+        filenameFormat: FilenameFormat
+    ): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd-HH-mm", Locale.getDefault())
+        val timestamp = dateFormat.format(metadata.date)
+        val filename = when(filenameFormat) {
+            TitleOnly -> metadata.title.take(245)
+            TimestampAndTitle -> "${timestamp}_${metadata.title.take(245 - (timestamp.length + 1))}"
+            TitleAndTimestamp -> "${metadata.title.take(245 - (timestamp.length + 1))}_$timestamp"
+        }
+
+        return "$filename.txt"
+    }
+}
+
+val artVandelayModule = module {
+    single<ArtVandelay> { new(::ArtVandelayImpl) }
+}
